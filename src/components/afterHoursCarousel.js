1f593b0578b28567cdcf6674d68cad6747f01191
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2024-11-26 18:13:47 -0500

    after hours

---
+++ src/components/afterHoursCarousel.js
@@ -0,0 +1,116 @@
+import { GatsbyImage } from "gatsby-plugin-image"
+import React, { useState, useRef } from "react"
+import Slider from "react-slick"
+import * as styles from "./afterHoursCarousel.module.css"
+
+const AfterHoursCarousel = ({ images }) => {
+  const [activeSlide, setActiveSlide] = useState(0)
+  const sliderRef = useRef(null)
+
+  const settings = {
+    slidesToShow: 1,
+    infinite: true,
+    useTransform: false,
+    dots: false,
+    arrows: false,
+    draggable: true,
+    afterChange: (current) => {
+      setActiveSlide(current)
+    },
+  }
+
+  return (
+    <div className={styles.simpleContainer}>
+      <Slider {...settings} className={styles.afterCarousel} ref={sliderRef}>
+        {images &&
+          images.map(image => {
+            return (
+              <div key={image.id} className={styles.slide}>
+                <div className={styles.slideContainer}>
+                  <figure>
+                    <GatsbyImage
+                      image={image.image?.gatsbyImageData}
+                      alt={image.image?.description}
+                      className={styles.carouselImage}
+                    ></GatsbyImage>
+                  </figure>
+                </div>
+              </div>
+            )
+          })}
+      </Slider>
+      <div className={styles.underCarousel}>
+        <div
+          className={styles.figcaption}
+          dangerouslySetInnerHTML={{
+            __html: images[
+              activeSlide
+            ].caption.childMarkdownRemark.html.replace(
+              /\b(\d+)\/(\d+)/g,
+              "<span class='fraction'><sup>$1</sup>&frasl;<sub>$2</sub></span>"
+            ),
+          }}
+        ></div>
+        <div className={styles.slideCountContainer}>
+          <div
+            role="button"
+            tabIndex={0}
+            aria-label="go to previous"
+            className={styles.previousArrow}
+            onClick={() => {
+              sliderRef.current.slickPrevious()
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className={styles.carouselSVG}
+              viewBox="0 0 13.047 28.672"
+            >
+              <path
+                id="Polygon_4"
+                data-name="Polygon 4"
+                d="M0,12.009,14.011,0,28.021,12.009"
+                transform="translate(0.659 28.346) rotate(-90)"
+                fill="none"
+                stroke="#fff"
+                stroke-width="1"
+              />
+            </svg>
+          </div>
+          {images && images?.length > 1 && (
+            <div className={styles.slideCount}>
+              {Math.round(activeSlide + 1)} / {images.length}
+            </div>
+          )}
+          <div
+            role="button"
+            tabIndex={0}
+            aria-label="go to next"
+            className={styles.nextArrow}
+            onClick={() => {
+              sliderRef.current.slickNext()
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className={styles.carouselSVG}
+              viewBox="0 0 13.047 28.672"
+            >
+              <path
+                id="Polygon_3"
+                data-name="Polygon 3"
+                d="M0,12.009,14.011,0,28.021,12.009"
+                transform="translate(12.389 0.325) rotate(90)"
+                fill="none"
+                stroke="#fff"
+                stroke-width="1"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AfterHoursCarousel
--- src/pages/after-hours.js
+++ src/pages/after-hours.js
@@ -1,11 +1,170 @@
-import React from "react"
+import React, { useState } from "react"
+import { graphql } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
+import * as styles from "../components/afterHours.module.css"
+import { Link, FormattedMessage } from "gatsby-plugin-intl"
+import SimpleCarousel from "../components/simpleCarousel"
+import VideoPlayer from "../components/videoPlayer"
+import AfterHoursCarousel from "../components/afterHoursCarousel"
 
-const AfterHours = () => {
+const AfterHours = ({ data, pageContext }) => {
+  const { entries } = data.allContentfulAfterHoursPage.nodes[0]
+
+  const [activeVideo, setActiveVideo] = useState(null)
+
+  const allData = entries
+  const featuredData = entries
+    .filter(entry => entry.isFeatured === true)
+    .map(entry => entry.entry)
+  const notFeaturedData = entries
+    .filter(entry => entry.isFeatured === false)
+    .map(entry => entry.entry)
+
   return (
     <div className="pageContainer">
-      <h1 className="pageHeading">After Hours</h1>
+      <h1 className="pageHeading">
+        <FormattedMessage id="after_hours"></FormattedMessage>
+      </h1>
+      <div className={styles.contentContainer}>
+        <div className={styles.featuredContainer}>
+          {featuredData.map((entry, index) => {
+            if (entry.videoId) {
+              return (
+                <VideoPlayer
+                  key={index}
+                  video={entry}
+                  videoId={entry.videoId}
+                  activeVideo={activeVideo}
+                  setActiveVideo={setActiveVideo}
+                ></VideoPlayer>
+              )
+            } else if (entry.carouselId) {
+              return (
+                <AfterHoursCarousel
+                  key={index}
+                  images={entry.images}
+                ></AfterHoursCarousel>
+              )
+            } else {
+              return (
+                <div key={index}>
+                  <a
+                    href={
+                      entry.isExternal
+                        ? entry.link
+                        : `${process.env.GATSBY_BASE_LINK}/${pageContext.language}${entry.link}`
+                    }
+                  >
+                    <GatsbyImage
+                      image={entry.image?.gatsbyImageData}
+                      alt={entry.image?.description}
+                      className={styles.imageLink}
+                    ></GatsbyImage>
+                    <div>{entry.title}</div>
+                  </a>
+                </div>
+              )
+            }
+          })}
+        </div>
+        <div className={styles.notFeaturedContainer}>
+          {notFeaturedData.map((entry, index) => {
+            if (entry.videoId) {
+              return (
+                <VideoPlayer
+                  key={index}
+                  video={entry}
+                  videoId={entry.videoId}
+                  activeVideo={activeVideo}
+                  setActiveVideo={setActiveVideo}
+                ></VideoPlayer>
+              )
+            } else if (entry.carouselId) {
+              return (
+                <AfterHoursCarousel
+                  key={index}
+                  images={entry.images}
+                ></AfterHoursCarousel>
+              )
+            } else {
+              return (
+                <div key={index}>
+                  <a
+                    href={
+                      entry.isExternal
+                        ? entry.link
+                        : `${process.env.GATSBY_BASE_LINK}/${pageContext.language}${entry.link}`
+                    }
+                  >
+                    <GatsbyImage
+                      image={entry.image?.gatsbyImageData}
+                      alt={entry.image?.description}
+                      className={styles.imageLink}
+                    ></GatsbyImage>
+                    <div>{entry.title}</div>
+                  </a>
+                </div>
+              )
+            }
+          })}
+        </div>
+      </div>
     </div>
   )
 }
+
+export const query = graphql`
+  query contentfulAfter($locale: String) {
+    allContentfulAfterHoursPage(filter: { node_locale: { eq: $locale } }) {
+      nodes {
+        id
+        entries {
+          entry {
+            ... on ContentfulAfterHoursCarousel {
+              carouselId: id
+              images {
+                id
+                caption {
+                  childMarkdownRemark {
+                    html
+                  }
+                }
+                image {
+                  description
+                  gatsbyImageData(layout: FULL_WIDTH)
+                }
+              }
+            }
+            ... on ContentfulLinkWithImage {
+              linkId: id
+              image {
+                description
+                gatsbyImageData(layout: FULL_WIDTH)
+              }
+              isExternal
+              link
+              title
+            }
+            ... on ContentfulVideoWrapper {
+              videoId: id
+              aspectRatio
+              caption {
+                childMarkdownRemark {
+                  html
+                }
+              }
+              coverImage {
+                description
+                gatsbyImageData(layout: FULL_WIDTH)
+              }
+              source
+            }
+          }
+          isFeatured
+        }
+      }
+    }
+  }
+`
 
 export default AfterHours
