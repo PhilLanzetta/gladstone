2c912ee422bcc4de6959428e78602f06a01cbd6e
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2024-11-27 16:50:22 -0500

    browser behavior

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,20 +4,9 @@ import CombinedProvider from "./src/context/CombinedProvider"
 
 export const wrapRootElement = CombinedProvider
 
-export const shouldUpdateScroll = ({
-  routerProps: { location },
-  getSavedScrollPosition,
-}) => {
-  // transition duration from `layout.js` * 1000 to get time in ms
-  // * 2 for exit + enter animation
-  const TRANSITION_DELAY = 0.3 * 1000 * 2
-  // if it's a "normal" route
-  if (location.action === "PUSH") {
-    window.setTimeout(() => window.scrollTo(0, 0), TRANSITION_DELAY)
-  }
-  // if we used the browser's forwards or back button
-  else {
-    window.setTimeout(() => window.scrollTo(0, 0), TRANSITION_DELAY)
-  }
-  return false
-}
+const transitionDelay = 250
+
+exports.shouldUpdateScroll = () => false
+
+exports.onRouteUpdate = () =>
+  window.setTimeout(() => window.scrollTo(0, 0), transitionDelay)
