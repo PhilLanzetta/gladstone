5cc842a628bbc829a8d0bca859729ad3051ccb8d
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2024-11-27 11:55:17 -0500

    after hours mobile

--- src/pages/after-hours.js
+++ src/pages/after-hours.js
@@ -2,13 +2,15 @@ import React, { useState } from "react"
 import { graphql } from "gatsby"
 import { GatsbyImage } from "gatsby-plugin-image"
 import * as styles from "../components/afterHours.module.css"
-import { Link, FormattedMessage } from "gatsby-plugin-intl"
-import SimpleCarousel from "../components/simpleCarousel"
+import { FormattedMessage } from "gatsby-plugin-intl"
 import VideoPlayer from "../components/videoPlayer"
 import AfterHoursCarousel from "../components/afterHoursCarousel"
+import useWindowSize from "../utils/useWindowSize"
 
 const AfterHours = ({ data, pageContext }) => {
   const { entries } = data.allContentfulAfterHoursPage.nodes[0]
+  const { width } = useWindowSize()
+  const isMobile = width < 701
 
   const allData = entries
   const featuredData = entries
@@ -18,7 +20,7 @@ const AfterHours = ({ data, pageContext }) => {
     .filter(entry => entry.isFeatured === false)
     .map(entry => entry.entry)
 
- const [activeVideo, setActiveVideo] = useState(featuredData[0].videoId)
+  const [activeVideo, setActiveVideo] = useState(featuredData[0].videoId)
 
   return (
     <div className="pageContainer">
@@ -26,97 +28,161 @@ const AfterHours = ({ data, pageContext }) => {
         <FormattedMessage id="after_hours"></FormattedMessage>
       </h1>
       <div className={styles.contentContainer}>
-        <div className={styles.featuredContainer}>
-          {featuredData.map((entry, index) => {
-            if (entry.videoId) {
-              return (
-                <VideoPlayer
-                  key={index}
-                  isFirst={index === 0}
-                  video={entry}
-                  videoId={entry.videoId}
-                  activeVideo={activeVideo}
-                  setActiveVideo={setActiveVideo}
-                ></VideoPlayer>
-              )
-            } else if (entry.carouselId) {
-              return (
-                <AfterHoursCarousel
-                  key={index}
-                  images={entry.images}
-                ></AfterHoursCarousel>
-              )
-            } else {
-              return (
-                <div key={index}>
-                  <a
-                    href={
-                      entry.isExternal
-                        ? entry.link
-                        : `${process.env.GATSBY_BASE_LINK}/${pageContext.language}${entry.link}`
-                    }
-                    target={entry.isExternal ? "_blank" : "_self"}
-                    rel="noreferrer"
-                  >
-                    <GatsbyImage
-                      image={entry.image?.gatsbyImageData}
-                      alt={entry.image?.description}
-                      className={styles.imageLink}
-                    ></GatsbyImage>
-                    <div>
-                      {entry.title} {entry.isExternal ? "  ↗" : ""}
+        {!isMobile && (
+          <>
+            <div className={styles.featuredContainer}>
+              {featuredData.map((entry, index) => {
+                if (entry.videoId) {
+                  return (
+                    <VideoPlayer
+                      key={index}
+                      isFirst={index === 0}
+                      video={entry}
+                      videoId={entry.videoId}
+                      activeVideo={activeVideo}
+                      setActiveVideo={setActiveVideo}
+                    ></VideoPlayer>
+                  )
+                } else if (entry.carouselId) {
+                  return (
+                    <AfterHoursCarousel
+                      key={index}
+                      images={entry.images}
+                    ></AfterHoursCarousel>
+                  )
+                } else {
+                  return (
+                    <div key={index}>
+                      <a
+                        href={
+                          entry.isExternal
+                            ? entry.link
+                            : `${process.env.GATSBY_BASE_LINK}/${pageContext.language}${entry.link}`
+                        }
+                        target={entry.isExternal ? "_blank" : "_self"}
+                        rel="noreferrer"
+                      >
+                        <GatsbyImage
+                          image={entry.image?.gatsbyImageData}
+                          alt={entry.image?.description}
+                          className={styles.imageLink}
+                        ></GatsbyImage>
+                        <div>
+                          {entry.title} {entry.isExternal ? "  ↗" : ""}
+                        </div>
+                      </a>
                     </div>
-                  </a>
-                </div>
-              )
-            }
-          })}
-        </div>
-        <div className={styles.notFeaturedContainer}>
-          {notFeaturedData.map((entry, index) => {
-            if (entry.videoId) {
-              return (
-                <VideoPlayer
-                  key={index}
-                  video={entry}
-                  videoId={entry.videoId}
-                  activeVideo={activeVideo}
-                  setActiveVideo={setActiveVideo}
-                ></VideoPlayer>
-              )
-            } else if (entry.carouselId) {
-              return (
-                <AfterHoursCarousel
-                  key={index}
-                  images={entry.images}
-                ></AfterHoursCarousel>
-              )
-            } else {
-              return (
-                <div key={index}>
-                  <a
-                    href={
-                      entry.isExternal
-                        ? entry.link
-                        : `${process.env.GATSBY_BASE_LINK}/${pageContext.language}${entry.link}`
-                    }
-                    target={entry.isExternal ? "_blank" : "_self"}
-                    rel="noreferrer"
-                  >
-                    <GatsbyImage
-                      image={entry.image?.gatsbyImageData}
-                      alt={entry.image?.description}
-                      className={styles.imageLink}
-                    ></GatsbyImage>
-                    <div>
-                      {entry.title} {entry.isExternal ? "  ↗" : ""}
+                  )
+                }
+              })}
+            </div>
+            <div className={styles.notFeaturedContainer}>
+              {notFeaturedData.map((entry, index) => {
+                if (entry.videoId) {
+                  return (
+                    <VideoPlayer
+                      key={index}
+                      video={entry}
+                      videoId={entry.videoId}
+                      activeVideo={activeVideo}
+                      setActiveVideo={setActiveVideo}
+                    ></VideoPlayer>
+                  )
+                } else if (entry.carouselId) {
+                  return (
+                    <AfterHoursCarousel
+                      key={index}
+                      images={entry.images}
+                    ></AfterHoursCarousel>
+                  )
+                } else {
+                  return (
+                    <div key={index}>
+                      <a
+                        href={
+                          entry.isExternal
+                            ? entry.link
+                            : `${process.env.GATSBY_BASE_LINK}/${pageContext.language}${entry.link}`
+                        }
+                        target={entry.isExternal ? "_blank" : "_self"}
+                        rel="noreferrer"
+                      >
+                        <GatsbyImage
+                          image={entry.image?.gatsbyImageData}
+                          alt={entry.image?.description}
+                          className={styles.imageLink}
+                        ></GatsbyImage>
+                        <div>
+                          {entry.title} {entry.isExternal ? "  ↗" : ""}
+                        </div>
+                      </a>
                     </div>
-                  </a>
-                </div>
-              )
-            }
-          })}
-        </div>
+                  )
+                }
+              })}
+            </div>
+          </>
+        )}
+        {isMobile && (
+          <div className={styles.mobileContainer}>
+            {allData.map((item, index) => {
+              if (item.entry.videoId) {
+                return (
+                  <div key={index} className={item.isFeatured ? styles.mobileFull : styles.mobileHalf}>
+                    <VideoPlayer
+                      isFirst={index === 0}
+                      video={item.entry}
+                      videoId={item.entry.videoId}
+                      activeVideo={activeVideo}
+                      setActiveVideo={setActiveVideo}
+                    ></VideoPlayer>
+                  </div>
+                )
+              } else if (item.entry.carouselId) {
+                return (
+                  <div
+                    key={index}
+                    className={
+                      item.isFeatured ? styles.mobileFull : styles.mobileHalf
+                    }
+                  >
+                    <AfterHoursCarousel
+                      images={item.entry.images}
+                    ></AfterHoursCarousel>
+                  </div>
+                )
+              } else {
+                return (
+                  <div
+                    key={index}
+                    className={
+                      item.isFeatured ? styles.mobileFull : styles.mobileHalf
+                    }
+                  >
+                    <a
+                      href={
+                        item.entry.isExternal
+                          ? item.entry.link
+                          : `${process.env.GATSBY_BASE_LINK}/${pageContext.language}${item.entry.link}`
+                      }
+                      target={item.entry.isExternal ? "_blank" : "_self"}
+                      rel="noreferrer"
+                    >
+                      <GatsbyImage
+                        image={item.entry.image?.gatsbyImageData}
+                        alt={item.entry.image?.description}
+                        className={styles.imageLink}
+                      ></GatsbyImage>
+                      <div>
+                        {item.entry.title} {item.entry.isExternal ? "  ↗" : ""}
+                      </div>
+                    </a>
+                  </div>
+                )
+              }
+            })}
+          </div>
+        )}
       </div>
     </div>
   )
