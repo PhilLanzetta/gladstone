ce5a2411230b876055ef8ac3aa24a9a330b4f252
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2024-11-27 18:30:40 -0500

    browser problems

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,21 +4,51 @@ import CombinedProvider from "./src/context/CombinedProvider"
 
 export const wrapRootElement = CombinedProvider
 
-const transitionDelay = 500
 
-export const shouldUpdateScroll = () => false
-
-export const onRouteUpdate = ({ location }) => {
-  if (location.hash) {
-    const target = document.getElementById(location.hash.slice(1))
-    const targetPosition =
-      target.getBoundingClientRect().top - 250 + window.scrollY
-    window.scrollTo({
-      top: targetPosition,
-    })
-  } else {
-    setTimeout(() => {
-      window.scrollTo(0, 0)
-    }, transitionDelay)
+export const shouldUpdateScroll = ({
+  routerProps: { location },
+  prevRouterProps,
+  getSavedScrollPosition,
+}) => {
+  // transition duration from `layout.js` * 1000 to get time in ms
+  // * 2 for exit + enter animation
+  const TRANSITION_DELAY = 500
+  // if it's a "normal" route
+  if (
+    prevRouterProps &&
+    prevRouterProps.location.pathname === location.pathname
+  ) {
+    return true
   }
+  if (location.action === "PUSH") {
+    if (location.hash) {
+      const target = document.getElementById(location.hash.slice(1))
+      const targetPosition =
+        target.getBoundingClientRect().top - 250 + window.scrollY
+      window.setTimeout(
+        () =>
+          window.scrollTo({
+            top: targetPosition,
+          }),
+        TRANSITION_DELAY
+      )
+    } else window.setTimeout(() => window.scrollTo(0, 0), TRANSITION_DELAY)
+  }
+  // if we used the browser's forwards or back button
+  else {
+    if (location.hash) {
+      const target = document.getElementById(location.hash.slice(1))
+      const targetPosition =
+        target.getBoundingClientRect().top - 250 + window.scrollY
+      window.scrollTo({
+        top: targetPosition,
+      })
+    } else {
+      window.setTimeout(
+        () => window.scrollTo(0, 0),
+        TRANSITION_DELAY
+      )
+    }
+  }
+  return false
 }
