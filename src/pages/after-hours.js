6e4c715f7ab3e7cede80b4a1c4a2777ee1beb91d
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2024-11-27 08:28:53 -0500

    vimeo test

--- src/pages/after-hours.js
+++ src/pages/after-hours.js
@@ -32,7 +32,6 @@ const AfterHours = ({ data, pageContext }) => {
               src="https://player.vimeo.com/video/536835102?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
               frameborder="0"
               allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
-              style="position:absolute;top:0;left:0;width:100%;height:100%;"
               title="Matthew Barney | The Cremaster Cycle - Trailer"
             ></iframe>
           </div>
