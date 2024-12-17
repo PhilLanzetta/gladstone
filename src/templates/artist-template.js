26797728f2b93f4ddeb6effc39d7da2ce532b562
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2024-12-17 15:29:10 -0500

    empty press

--- src/templates/artist-template.js
+++ src/templates/artist-template.js
@@ -37,7 +37,7 @@ const Artist = ({ data }) => {
     ?.map(handle => allPublications?.filter(pub => pub.handle === handle))
     .flat()
 
-  const initialPress = press.slice(0, 8)
+  const initialPress = press?.slice(0, 8)
   const morePress = press.length > 8
 
   const { width } = useWindowSize()
