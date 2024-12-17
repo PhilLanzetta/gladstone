b7519fd01373616d19ca60ad44fff71088e426a9
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2024-12-17 12:50:55 -0500

    empty fair node

--- gatsby-node.js
+++ gatsby-node.js
@@ -159,7 +159,7 @@ exports.createPages = async ({ actions, graphql }) => {
   })
 
   viewingRooms.forEach(({ node }) => {
-    const fairSlug = node.fair[0].slug
+    const fairSlug = node.fair && node.fair[0].slug
     createPage({
       path: `/fair/${fairSlug}/viewing-room`,
       component: require.resolve("./src/templates/viewingRoom-template.js"),
